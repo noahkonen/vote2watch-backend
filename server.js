62054f8fe3e3ee9028b3b27897c80b59e9462081
Noah <59939241+noahkonen@users.noreply.github.com>
2021-03-04 17:20:13 -0500

    Added the git ignore file, also installed express

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3003;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// listening to port
+app.listen(PORT, () => {
+    console.log("Listening to Port", PORT);
+});
